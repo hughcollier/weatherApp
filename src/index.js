3fcc3e9c4163b47a9c7c5ac966d5d3451beb46ca
Hugh Collier <collier.289@gmail.com>
2024-01-09 21:57:46 +0000

    WIP - Updated fetch to use promise.all to fetch data from multiple endpoints

--- src/index.js
+++ src/index.js
@@ -7,35 +7,63 @@ const getCurrenLocationButton = document.getElementById(
   'getCurrenLocationButton'
 )
 const weatherEl = document.getElementById('weather')
-const url = new URL('https://api.weatherapi.com/v1/current.json')
-let searchString
+const url = new URL('https://api.weatherapi.com/v1')
+let searchString, currentWeatherUrl, astronomyUrl, weather, forecastUrl
 
-const getWeatherData = async function () {
+// I can actually get all the data I need from the /forecast endpoint but since this is a learning project I'm going to fetch the current weather and astronomy data from separate endpoints so I can get get some practice using Promise.all()
+
+const getWeatherData = async function (
+  currentWeatherUrl,
+  astronomyUrl,
+  forecastUrl
+) {
   try {
-    let response = await fetch(url)
-    let dataObject = await response.json()
-    if (!response.ok) throw new Error('Something went wrong.')
-    displayResult(dataObject)
+    let [weather, moon, forecast] = await Promise.all([
+      fetch(currentWeatherUrl),
+      fetch(astronomyUrl),
+      fetch(forecastUrl),
+    ])
+
+    let weatherDataObject = await weather.json()
+    console.log(weatherDataObject)
+    let moonDataObject = await moon.json()
+    console.log(moonDataObject)
+    let forecastDataObject = await forecast.json()
+    console.log(forecastDataObject)
+
+    if (!weather.ok || !moon.ok) throw new Error('Something went wrong.')
+
+    displayResult(weatherDataObject)
   } catch (error) {
     console.warn(error.message)
   }
 }
-const displayResult = function (dataObject) {
-  console.log(dataObject)
+const displayResult = function (weatherDataObject) {
   weatherEl.textContent = `The weather in ${
-    dataObject.location.name
-  } is ${dataObject.current.condition.text.toLowerCase()} and the temperature is ${
-    dataObject.current.temp_c
+    weatherDataObject.location.name
+  } is ${weatherDataObject.current.condition.text.toLowerCase()} and the temperature is ${
+    weatherDataObject.current.temp_c
   }ºC`
 }
 
-const buildSearchString = function (searchString) {
-  url.search = `?key=${process.env.API_KEY}&q=${searchString}&aqi=no`
+const constructUrl = function (endpoint, searchString) {
+  url.search = `?key=${process.env.API_KEY}&q=${searchString}`
+  url.pathname = '/v1'
+  url.pathname += endpoint
+  return url
 }
 
 const getCurrentLocation = function (position) {
-  buildSearchString(`${position.coords.latitude}, ${position.coords.longitude}`)
-  getWeatherData()
+  searchString = `${position.coords.latitude},${position.coords.longitude}`
+  currentWeatherUrl = constructUrl('/current.json', searchString)
+  currentWeatherUrl += '&aqi=no'
+
+  astronomyUrl = constructUrl('/astronomy.json', searchString)
+
+  forecastUrl = constructUrl('/forecast.json', searchString)
+  forecastUrl += '&days=3&aqi=no&alerts=no'
+
+  getWeatherData(currentWeatherUrl, astronomyUrl, forecastUrl)
 }
 
 const handleGeolocationError = function (error) {
@@ -56,6 +84,14 @@ if ('geolocation' in navigator) {
 }
 
 submitBtn.addEventListener('click', () => {
-  buildSearchString(document.getElementById('location').value)
-  getWeatherData()
+  searchString = document.getElementById('location').value
+  currentWeatherUrl = constructUrl('/current.json', searchString)
+  currentWeatherUrl += '&aqi=no'
+
+  astronomyUrl = constructUrl('/astronomy.json', searchString)
+
+  forecastUrl = constructUrl('/forecast.json', searchString)
+  forecastUrl += '&days=3&aqi=no&alerts=no'
+
+  getWeatherData(currentWeatherUrl, astronomyUrl, forecastUrl)
 })
